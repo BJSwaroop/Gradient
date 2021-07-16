f8a0118f5461a762bc3ce4e7861a304c03162e3c
BJSwaroop <BJSwaroop1@gmail.com>
2021-07-16 09:51:50 +0530

    modified icons

--- home.js
+++ home.js
@@ -1,4 +1,11 @@
 
+function openNav() {
+  document.getElementById("mobile__menu").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("mobile__menu").style.width = "0";
+}
 // For corousel of the testimonials
 
 var slideIndex = 1;
