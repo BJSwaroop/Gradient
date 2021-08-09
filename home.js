f8c9bc6b0470c78d7d6227dfaeb8c5fcb1c59716
BJSwaroop <BJSwaroop1@gmail.com>
2021-08-09 18:52:37 +0530

    modified content

--- home.js
+++ home.js
@@ -45,7 +45,8 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" dotactive", "");
 }
-  slides[slideIndex - 1].style.display = "flex";
+slides[slideIndex - 1].style.display = "flex";
+  slides[slideIndex - 1].style.transform = "translateY(10px)";
   dots[slideIndex-1].className += " dotactive";
 
 }
