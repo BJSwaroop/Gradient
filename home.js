e1d6bd07eb68339d87dd514f4c60ddb081840d78
BJSwaroop <BJSwaroop1@gmail.com>
2021-08-30 17:50:53 +0530

    added animations

--- home.js
+++ home.js
@@ -14,43 +14,105 @@ function openNav() {
 function closeNav() {
   document.getElementById("mobile__menu").style.width = "0";
 }
+// For team cards
+const teamSlide = document.querySelectorAll(".team-card");
+const teamSlides = teamSlide.length;
+var currentTeamSlide = 0;
+var teamSlider;
+var teamRepeater = () =>{
+   teamSlider = setInterval(function(){
+     currentTeamSlide++;
+     if(currentTeamSlide>teamSlides-3){
+         currentTeamSlide = 0;
+     }
+     teamSlide.forEach((tslide) => {
+       tslide.classList.remove("active");
+     });
+     teamSlide[currentTeamSlide].classList.add("active");
+     teamSlide[currentTeamSlide+1].classList.add("active");
+     teamSlide[currentTeamSlide+2].classList.add("active");
+   },3000);
+}
+teamRepeater();
 // For corousel of the testimonials
-
-var slideIndex = 1;
-showSlides(slideIndex);
-//function for Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+const slides = document.querySelectorAll(".tcard");
+const dots = document.querySelectorAll(".dot");
+const numberofSlides = slides.length;
+const nextSlide = document.querySelector(".next");
+const prevSlide = document.querySelector(".prev");
+var slideNumber = 0;
+function currentSlide(cur){
+   slideNumber = cur-1;
+   slides.forEach((slide) => {
+     slide.classList.remove("active");
+   });
+   dots.forEach((dot) => {
+     dot.classList.remove("dotactive");
+   });
+   slides[slideNumber].classList.add("active");
+   dots[slideNumber].classList.add("dotactive");
 }
-// function for Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("tcard");
-  var dots = document.getElementsByClassName("dot");
-
-  if (n > slides.length) {
-    slideIndex = 1
+// for next slides
+nextSlide.addEventListener('click',() =>{
+  slideNumber++;
+  if(slideNumber>numberofSlides-1){
+      slideNumber = 0;
   }
-  if (n < 1) {
-    slideIndex = slides.length
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+  dots.forEach((dot) => {
+    dot.classList.remove("dotactive");
+  });
+  slides[slideNumber].classList.add("active");
+  dots[slideNumber].classList.add("dotactive");
+})
+// for previous slides
+prevSlide.addEventListener("click",()=>{
+  slideNumber--;
+  if(slideNumber<0){
+    slideNumber = numberofSlides-1;
   }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+  slides.forEach(slide => {
 
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" dotactive", "");
+    slide.classList.remove("active");
+  })
+  dots.forEach(dot => {
+
+    dot.classList.remove("dotactive");
+  })
+  slides[slideNumber].classList.add("active");
+  dots[slideNumber].classList.add("dotactive");
+})
+// to make it infinite
+var playSlider;
+var repeater = () =>{
+   playSlider = setInterval(function(){
+     slideNumber++;
+     if(slideNumber>numberofSlides-1){
+         slideNumber = 0;
+     }
+     slides.forEach((slide) => {
+       slide.classList.remove("active");
+     });
+     dots.forEach((dot) => {
+       dot.classList.remove("dotactive");
+     });
+     slides[slideNumber].classList.add("active");
+     dots[slideNumber].classList.add("dotactive");
+   },3000);
 }
-slides[slideIndex - 1].style.display = "flex";
-  slides[slideIndex - 1].style.transform = "translateY(10px)";
-  dots[slideIndex-1].className += " dotactive";
+repeater();
+const slider = document.querySelector(".testimonials-corousel");
+    //stop the image slider autoplay on mouseover
+    slider.addEventListener("mouseover", () => {
+      clearInterval(playSlider);
+    });
 
-}
-
+    //start the image slider autoplay again on mouseout
+    slider.addEventListener("mouseout", () => {
+      repeater();
+    });
 // for the tabs in the protfolio section by calling on click
 function active(n) {
   const protfolio = document.getElementsByClassName("tbtn");
